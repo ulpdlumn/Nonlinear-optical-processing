0b0dbebbb4a8293df53e02f66dcfa0d72d8aa8dc
wadsw053 <133675750+wadsw053@users.noreply.github.com>
2025-08-29 17:05:19 -0500

    reading files from a folder

--- Polarization plots.py
+++ Polarization plots.py
@@ -1,13 +1,24 @@
 #code to make polarization plot
 #import packages
 
-#read data, please work
-network_path = "C:\Users\L136_L2\Documents\Andor Solis"
-try:
-    with open(network_path, 'r') as f:
-        content = f.read()
-        print(content)
-except FileNotFoundError:
-    print(f"Error: File not found at {network_path}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+#read
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+#where files live
+folder_path = 'C:/Users/L136_L2/Documents/8262026'
+#list all csv files in the folder
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+#read and concatenate data:loop through each csv file, read it into a pandas data frame and append it into a list
+all_dataframes = []
+for csv_file in csv_files:
+    file_path = os.path.join(folder_path, csv_file)
+    df = pd.read_csv(file_path)
+    all_dataframes.append(df)
+
+combined_df = pd.concat(all_dataframes, ignore_index=True)
+
+#convert to a numpy array
+data_array = combined_df.values
+print(data_array)
+plt.plot(data_array)
